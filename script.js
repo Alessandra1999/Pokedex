765246783d13c2416ac5d218d8c057aa634978bf
Carlos Henrique <158006737+carlooosdev@users.noreply.github.com>
2024-01-29 17:03:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,19 +7,26 @@ const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
+let pokemonList = [];
+
+const fetchPokemonList = async () => {
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const data = await APIResponse.json();
+    pokemonList = data.results.map(pokemon => pokemon.name);
+};
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-        if (APIResponse.status === 200) {
-            const data = await APIResponse.json();
-            return data;
-        }
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
-   pokemonName.innerHTML = 'Loading...';
-   pokemonNumber.innerHTML = '';
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
     const data = await fetchPokemon(pokemon);
 
     if (data) {
@@ -33,23 +40,34 @@ const renderPokemon = async (pokemon) => {
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not Found :c';
         pokemonNumber.innerHTML = '';
-    } 
+    }
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    renderPokemon(input.value.toLowerCase());
+    const inputValue = input.value.toLowerCase();
+    
+    const filteredPokemon = pokemonList.filter(pokemon => pokemon.toLowerCase().includes(inputValue));
+    
+    if (filteredPokemon.length > 0) {
+        renderPokemon(filteredPokemon[0]);
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not Found :c';
+        pokemonNumber.innerHTML = '';
+    }
 });
 
 buttonPrev.addEventListener('click', () => {
     if (searchPokemon > 1) {
         searchPokemon -= 1;
-    renderPokemon(searchPokemon);
+        renderPokemon(searchPokemon);
     }
 });
 
 buttonNext.addEventListener('click', () => {
     searchPokemon += 1;
     renderPokemon(searchPokemon);
-    });
-renderPokemon(searchPokemon);
+});
+
+fetchPokemonList().then(() => renderPokemon(searchPokemon));
